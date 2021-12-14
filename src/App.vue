48b048dacd00efdb61ba5bfdb73810911e0cf710
pooh7159 <pooh6043@gmail.com>
2021-12-14 17:41:44 +0900

    todo app part 1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <div id="app">
-   
+  <div id="app" class="container">
+    <h1 class="text-center">Todo app</h1>
+    <input type="text" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo" v-model="todoText">
+    <hr>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
   </div>
 </template>
 
 <script>
 
-
+import Todo from '@/components/Todo.vue'
 export default {
- 
+  components : {
+    Todo
+  },
+  data(){
+    return{
+      todos : [
+        { id : 1, text: 'buy a car', checked : false},
+        { id : 2, text: 'play game', checked : false}
+      ],
+      todoText : ''
+    }
+  },
+  methods : {
+    addTodo(e){
+      this.todos.push({
+        id : Math.random(),
+        text : e.target.value,
+        checked : false
+      })
+      this.todoText = '';
+    }
+  }
+
 }
 </script>
 
